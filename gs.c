4630bc653e7ca74523a4391adb824c2e179e6c2d
Muhammad Joyo <muhammadjoyo@172-16-7-232.DYNAPOOL.NYU.EDU>
2017-03-07 00:30:53 -0500

    possible fix to send

--- gs.c
+++ gs.c
@@ -286,8 +286,15 @@ int main(int argc, char *argv[]) {
 
       //printf("Still good ERROR CHECK\n");
 
+      float *temp;
+
       for(int p = 1; p < comm_sz; p++) {
-        MPI_Recv(&new + (sizeof(float) * local_num * p), local_num, MPI_FLOAT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(temp/*&new + (sizeof(float) * local_num * p)*/, local_num, MPI_FLOAT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int counter = 0;
+        for(int i = local_num * p; i < local_num * (p + 1); i++) {
+          new[i] = temp[counter];
+          counter++;
+        }
         MPI_Recv(&procs_done + (sizeof(int) * p), 1, MPI_INT, p, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Received\n");
       }
