19f0bd72fc6ae73ad6e2aecb25d2e2a3c6c114e3
Muhammad Joyo <muhammadjoyo@172-16-7-232.DYNAPOOL.NYU.EDU>
2017-03-07 00:47:54 -0500

    possible fix to send

--- gs.c
+++ gs.c
@@ -286,16 +286,20 @@ int main(int argc, char *argv[]) {
 
       //printf("Still good ERROR CHECK\n");
 
-      float *temp;
+      float *temp = (float *) malloc(local_num * sizeof(float));
+      if( !temp) {
+        printf("Cannot allocate new!\n");
+        exit(1);
+      }
       int status;
 
       for(int p = 1; p < comm_sz; p++) {
         MPI_Recv(&temp/*&new + (sizeof(float) * local_num * p)*/, local_num, MPI_FLOAT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         int counter = 0;
-        // for(int i = local_num * p; i < local_num * (p + 1); i++) {
-        //   new[i] = temp[counter];
-        //   counter++;
-        // }
+        for(int i = local_num * p; i < local_num * (p + 1); i++) {
+          new[i] = temp[counter];
+          counter++;
+        }
         MPI_Recv(&status, 1, MPI_INT, p, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         procs_done[p] = status;
         printf("Received\n");
