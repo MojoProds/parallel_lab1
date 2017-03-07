be06cf38aa92e255ecca310249eed0dba3110850
Muhammad Joyo <muhammadjoyo@172-16-7-232.DYNAPOOL.NYU.EDU>
2017-03-06 22:05:14 -0500

    changed MPI INIT to NULL NULL

--- gs.c
+++ gs.c
@@ -194,7 +194,7 @@ int main(int argc, char *argv[]) {
   check_matrix();
 
   //Initialize MPI
-  MPI_Init(&argc, &argv);
+  MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   local_num = num / comm_sz;
