b132e1b556f13e745be40f40dc01a75c4a323985
Muhammad Joyo <muhammadjoyo@172-17-49-64.DYNAPOOL.NYU.EDU>
2017-03-08 18:01:58 -0500

    possible array send fix

--- gs.c
+++ gs.c
@@ -291,7 +291,7 @@ int main(int argc, char *argv[]) {
 
       //printf("Still good ERROR CHECK\n");
 
-      float *temp;// = (float *) malloc(local_num * sizeof(float));
+      float **temp;// = (float *) malloc(local_num * sizeof(float));
       int status;
 
       for(int p = 1; p < comm_sz; p++) {
