4deefb52d531c3067f5f18e7d478b6eb1748a516
Muhammad Joyo <muhammadjoyo@172-17-49-64.DYNAPOOL.NYU.EDU>
2017-03-08 19:45:08 -0500

    works the same for multiple processes as for 1 process

--- gs.c
+++ gs.c
@@ -158,7 +158,7 @@ void get_input(char filename[]) {
  */
 int within_error(float newVal, float oldVal) {
   float error = fabs((newVal - oldVal) / newVal);
-  if(error <= err) {
+  if(error < err) {
     return 1;
   } else {
     return 0;
